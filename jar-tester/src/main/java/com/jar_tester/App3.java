5500ab8b6e340af9d5e587ad6d245d2578bc00ad
alessandro.garonzi <alessandro.garonzi@CPX-HXJSQZEY5DA.dir.svc.accenture.com>
2019-05-27 17:19:11 +0200

    Commit space final

--- jar-tester/src/main/java/com/jar_tester/App3.java
+++ jar-tester/src/main/java/com/jar_tester/App3.java
@@ -8,6 +8,6 @@ public class App3
 {
     public static void main( String[] args )
     {
-        System.out.println( "This is the third tester class" );
+        System.out.println( "This is the third tester class " );
     }
 }
