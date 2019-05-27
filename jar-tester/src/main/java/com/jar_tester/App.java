43c4ef9324acda40e6427d890f4df879df6673f4
alessandro.garonzi <alessandro.garonzi@CPX-HXJSQZEY5DA.dir.svc.accenture.com>
2019-05-27 16:11:04 +0200

    Initial set of object

---
+++ jar-tester/src/main/java/com/jar_tester/App.java
@@ -0,0 +1,15 @@
+package com.jar_tester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is the first tester class" ); 
+        System.out.println( "Test" );
+        System.out.println( "Test2" ); 
+    }
+}
